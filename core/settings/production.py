b137af6c94f5e7e38f4eae8906e4daa9734dd508
applewebbo <83427957+applewebbo@users.noreply.github.com>
2022-10-19 18:41:10 +0200

    changed db settings

--- core/settings/production.py
+++ core/settings/production.py
@@ -23,9 +23,9 @@ ALLOWED_HOSTS = [env("ALLOWED_HOST")]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": 'env("DB_NAME")',
-        "USER": 'env("DB_USER")',
-        "PASSWORD": 'env("DB_PASSWORD")',
-        "HOST": 'env("DB_HOST")',
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
     }
 }

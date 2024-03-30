63859f501ac41aca66ad4d72fb7f6ea8a3236112
WEBBOgrafico <e.bonardi@me.com>
2024-03-30 10:46:46 +0100

    added django-allauth

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path("", include("school_menu.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

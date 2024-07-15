68db7dcf0ab451a34d9254731600e7e2e7a00dc2
WEBBOgrafico <e.bonardi@me.com>
2024-07-15 14:54:56 +0200

    added configuration for transactional email using anymail[mailgun] (for #33)

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -4,6 +4,7 @@ DEBUG = True
 NO_RELOAD = False
 
 INSTALLED_APPS += [
+    "anymail",
     "django_browser_reload",
     "django_extensions",
 ]
@@ -27,3 +28,16 @@ ALLOWED_HOSTS = []
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+# DJANGO_ANYMAIL
+EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+DEFAULT_FROM_EMAIL = "info@mg.webbografico.com"
+
+ANYMAIL = {
+    "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
+    "MAILGUN_API_URL": env("MAILGUN_API_URL"),
+    "MAILGUN_SENDER_DOMAIN": env("MAILGUN_SENDER_DOMAIN"),
+}
+
+# DJANGO_ALLAUTH
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"

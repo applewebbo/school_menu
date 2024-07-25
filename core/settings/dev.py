b02f928882a60371e76930020d71a0507d096446
WEBBOgrafico <e.bonardi@me.com>
2024-07-25 15:02:57 +0200

    added settings for db backup on dropbox in settings.dev

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -28,3 +28,16 @@ ALLOWED_HOSTS = []
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+INSTALLED_APPS += [
+    "dbbackup",
+]
+
+# DBBACKUP
+DBBACKUP_STORAGE = "storages.backends.dropbox.DropBoxStorage"
+DBBACKUP_STORAGE_OPTIONS = {
+    "oauth2_access_token": env("DROPBOX_OAUTH2_ACCESS_TOKEN"),
+    "oauth2_refresh_token": env("DROPBOX_OAUTH2_REFRESH_TOKEN"),
+    "app_secret": env("DROPBOX_APP_SECRET"),
+    "app_key": env("DROPBOX_APP_KEY"),
+}

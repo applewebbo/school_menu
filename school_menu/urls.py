5bb7f67449aaac869bb4916826b1b2134d422dfd
WEBBOgrafico <e.bonardi@me.com>
2024-08-13 16:41:36 +0200

    added initial version of help page with images (for #50)

--- school_menu/urls.py
+++ school_menu/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
         name="get_menu",
     ),
     path("info", TemplateView.as_view(template_name="pages/info.html"), name="info"),
+    path("help", TemplateView.as_view(template_name="pages/help.html"), name="help"),
     # TODO: get this url back when ISSUE #34 is implemented
     # path("json_menu", views.json_menu, name="json_menu"),
     path("menu/<slug:slug>/", views.school_menu, name="school_menu"),

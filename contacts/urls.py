aee14d973783d288cf715e93e1ac0a596be8d933
WEBBOgrafico <e.bonardi@me.com>
2024-08-21 11:47:14 +0200

    added placeholder contacts links in footer and navbar (for #61)

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,6 +1,12 @@
+from django.urls import path
+
+from . import views
+
 app_name = "contacts"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.contact, name="contact"),
+]
 
 htmx_urlpatterns = []
 

c551a5c004a493caf66296d398aacbfa829814ea
WEBBOgrafico <e.bonardi@me.com>
2024-08-21 10:13:12 +0200

    simplified static pages and added privacy placeholder (for #60)

--- school_menu/urls.py
+++ school_menu/urls.py
@@ -16,16 +16,6 @@ urlpatterns = [
     ),
     path("info", TemplateView.as_view(template_name="pages/info.html"), name="info"),
     path("help", TemplateView.as_view(template_name="pages/help.html"), name="help"),
-    path(
-        "disclaimer",
-        TemplateView.as_view(template_name="pages/disclaimer.html"),
-        name="disclaimer",
-    ),
-    path(
-        "credits",
-        TemplateView.as_view(template_name="pages/credits.html"),
-        name="credits",
-    ),
     # TODO: get this url back when ISSUE #34 is implemented
     # path("json_menu", views.json_menu, name="json_menu"),
     path("menu/<slug:slug>/", views.school_menu, name="school_menu"),

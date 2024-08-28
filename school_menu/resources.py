a2200cd95df1db9c24e123dade1741a0503c147d
WEBBOgrafico <e.bonardi@me.com>
2024-08-28 16:35:36 +0200

    completed menu_export (for #71)

--- school_menu/resources.py
+++ school_menu/resources.py
@@ -1,4 +1,5 @@
 from import_export import resources
+from import_export.fields import Field
 
 from .models import DetailedMeal, SimpleMeal
 
@@ -9,6 +10,14 @@ class DetailedMealResource(resources.ModelResource):
 
     # def before_save_instance(self, instance, row, **kwargs):
     #     instance.school_id = self.school_id
+
+    day = Field(attribute="get_day_display", column_name="giorno")
+    week = Field(attribute="week", column_name="settimana")
+    first_course = Field(attribute="first_course", column_name="primo")
+    second_course = Field(attribute="second_course", column_name="secondo")
+    side_dish = Field(attribute="side_dish", column_name="contorno")
+    fruit = Field(attribute="fruit", column_name="frutta")
+    snack = Field(attribute="snack", column_name="spuntino")
 
     class Meta:
         model = DetailedMeal
@@ -24,6 +33,11 @@ class DetailedMealResource(resources.ModelResource):
 
 
 class SimpleMealResource(resources.ModelResource):
+    day = Field(attribute="get_day_display", column_name="giorno")
+    week = Field(attribute="week", column_name="settimana")
+    menu = Field(attribute="menu", column_name="pranzo")
+    snack = Field(attribute="snack", column_name="spuntino")
+
     class Meta:
         model = SimpleMeal
         fields = ("week", "day", "menu", "snack")

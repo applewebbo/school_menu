561f7a1a1765c74062050967e5d6380084be8439
applewebbo <e.bonardi@me.com>
2025-08-28 08:35:50 +0200

    fix(accessibility) Add labels to AnonymousNotificationForm fields (for #152)

--- notifications/forms.py
+++ notifications/forms.py
@@ -10,16 +10,18 @@ class AnonymousMenuNotificationForm(forms.ModelForm):
     class Meta:
         model = AnonymousMenuNotification
         fields = ["school", "notification_time", "subscription_info"]
-        widgets = {"subscription_info": forms.HiddenInput()}
+        widgets = {
+            "subscription_info": forms.HiddenInput(),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_tag = False
-        self.fields["school"].label = "Di quale scuola vuoi conoscere il menu?"
+        self.fields["school"].label = "Scuola"
         self.fields["school"].queryset = School.objects.filter(
             is_published=True
         ).order_by("name")
         self.fields["school"].label_from_instance = lambda obj: obj.name
         self.fields["school"].empty_label = None
-        self.fields["notification_time"].label = "Quando vuoi ricevere la notifica?"
+        self.fields["notification_time"].label = "Orario di notifica"
